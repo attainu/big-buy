cecdc6fbd1a35a272a3625e78ff4fcd48c4ebdea
Gorisab Dodamani <gorisab2@gmail.com>
2020-06-21 23:26:56 +0530

    Adding nodemailer

---
+++ views/partials/greet.js
@@ -0,0 +1,11 @@
+const wish = `Hi There,
+      <br/>
+      Thank you for registering!
+      <br/><br/>
+      <h1> Welcome to Big Buy <h1>
+      <br/>
+      <h1><img src="https://cdn.photographylife.com/wp-content/uploads/2012/07/BigBuy.jpg" width="100px" height="100px"></h1>      
+      <br/><br/>
+      Have a pleasant day.`;
+
+module.exports = wish;
